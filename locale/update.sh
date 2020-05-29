8e96a0eca1bad1efa494a6c2cb3aa138659bd914
Tetsuo Koyama <tkoyama010@gmail.com>
2020-05-30 08:22:39 +0900

    :new: update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -5,14 +5,14 @@ set -ex
 
 # required environment variables
 # SPHINXINTL_TRANSIFEX_USERNAME=tkoyama010
-# SPHINXINTL_TRANSIFEX_PROJECT_NAME=mayavi
+# SPHINXINTL_TRANSIFEX_PROJECT_NAME=tvtk-doc
 LOCAL_PYTHON_PATH="/home/runner/.local/bin"
 
 # pull po files from transifex
 cd `dirname $0`
 $LOCAL_PYTHON_PATH/sphinx-intl create-transifexrc
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../mayavi/docs/source/mayavi pot
+$LOCAL_PYTHON_PATH/sphinx-build -T -b gettext ../mayavi/docs/source/tvtk pot
 $LOCAL_PYTHON_PATH/sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
 $LOCAL_PYTHON_PATH/tx push -s --skip
